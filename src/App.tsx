a9d7132a458270b840674b2956944c36f16d57c1
Lucas Duarte <lucasduartedev@gmail.com>
2025-11-21 15:08:59 -0300

    Aplicando Use State

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { Header } from './components/header'
 import { Aluno } from './components/aluno'
 import { Footer } from './components/footer.tsx'
+import { useState } from 'react'
 
 import './app.css'
 
 export default function App() {
+
+  const [input, setInput] = useState("");
+  const [aluno, setAluno] = useState("");
+
+  function mostrarAluno() {
+    setAluno(input);
+  }
+
   return(
     <div>
       <Header title="Título principal"/>
@@ -12,6 +21,21 @@ export default function App() {
       <Aluno nome="Amanda" idade={15} />
       <Aluno nome="Julia" idade={16} />
       <Aluno nome="João" idade={14} />
+
+      <br />
+      <br />
+      <input
+        type="text"
+        placeholder="Digite seu nome"
+        value={input}
+        onChange={ (e) => setInput(e.target.value)}
+      />
+
+      <br />
+      <button onClick={mostrarAluno}>Mostrar Nome</button>
+
+      <hr />
+      <h3>Bem-vindo: {aluno}</h3>
 
       <Footer/>
     </div>
